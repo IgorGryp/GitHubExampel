b6e3db64708742eadc724b502a92a9c16463a901
Igor Grypishyn <igor.gryp@gmail.com>
2021-02-15 08:50:57 +0100

    Initiering comit. Lägger till projektet till repository

---
+++ src/github/exempel/pkg1/GitHubExempel1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github.exempel.pkg1;
+
+/**
+ *
+ * @author igorg
+ */
+public class GitHubExempel1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Detaa är ett GitHub Exempel!");
+    }
+    
+}
